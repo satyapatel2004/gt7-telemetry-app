3364dd0e2c556295caeec87b03f58f4eb3f1f488
Satya Patel <satyapatel@clinials.com>
2024-07-19 17:23:21 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,255 @@
+import datetime
+import json
+import logging
+import math
+import socket
+import struct
+import time
+import copy
+import sys 
+import curses 
+import traceback
+from datetime import timedelta
+from threading import Thread
+from typing import List
+
+from salsa20 import Salsa20_xor
+
+class Data:
+    """ Object that represent the data being read from each packet """
+    def __init__(self,ddata): 
+        if not ddata: return 
+        
+        self.package_id = struct.unpack('i', ddata[0x70:0x70 + 4])[0]
+        self.best_lap = struct.unpack('i', ddata[0x78:0x78 + 4])[0]
+        self.last_lap = struct.unpack('i', ddata[0x7C:0x7C + 4])[0]
+        self.current_lap = struct.unpack('h', ddata[0x74:0x74 + 2])[0]
+        self.current_gear = struct.unpack('B', ddata[0x90:0x90 + 1])[0] & 0b00001111
+        self.suggested_gear = struct.unpack('B', ddata[0x90:0x90 + 1])[0] >> 4
+        self.fuel_capacity = struct.unpack('f', ddata[0x48:0x48 + 4])[0]
+        self.current_fuel = struct.unpack('f', ddata[0x44:0x44 + 4])[0]  # fuel
+        self.boost = struct.unpack('f', ddata[0x50:0x50 + 4])[0] - 1
+
+        self.tyre_diameter_FL = struct.unpack('f', ddata[0xB4:0xB4 + 4])[0]
+        self.tyre_diameter_FR = struct.unpack('f', ddata[0xB8:0xB8 + 4])[0]
+        self.tyre_diameter_RL = struct.unpack('f', ddata[0xBC:0xBC + 4])[0]
+        self.tyre_diameter_RR = struct.unpack('f', ddata[0xC0:0xC0 + 4])[0]
+
+        self.type_speed_FL = abs(3.6 * self.tyre_diameter_FL * struct.unpack('f', ddata[0xA4:0xA4 + 4])[0])
+        self.type_speed_FR = abs(3.6 * self.tyre_diameter_FR * struct.unpack('f', ddata[0xA8:0xA8 + 4])[0])
+        self.type_speed_RL = abs(3.6 * self.tyre_diameter_RL * struct.unpack('f', ddata[0xAC:0xAC + 4])[0])
+        self.tyre_speed_RR = abs(3.6 * self.tyre_diameter_RR * struct.unpack('f', ddata[0xB0:0xB0 + 4])[0])
+
+        self.car_speed = 3.6 * struct.unpack('f', ddata[0x4C:0x4C + 4])[0]
+
+        if self.car_speed > 0:
+            self.tyre_slip_ratio_FL = '{:6.2f}'.format(self.type_speed_FL / self.car_speed)
+            self.tyre_slip_ratio_FR = '{:6.2f}'.format(self.type_speed_FR / self.car_speed)
+            self.tyre_slip_ratio_RL = '{:6.2f}'.format(self.type_speed_RL / self.car_speed)
+            self.tyre_slip_ratio_RR = '{:6.2f}'.format(self.tyre_speed_RR / self.car_speed)
+
+        self.time_on_track = timedelta(
+            seconds=round(struct.unpack('i', ddata[0x80:0x80 + 4])[0] / 1000))  # time of day on track
+
+        self.total_laps = struct.unpack('h', ddata[0x76:0x76 + 2])[0]  # total laps
+
+        self.current_position = struct.unpack('h', ddata[0x84:0x84 + 2])[0]  # current position
+        self.total_positions = struct.unpack('h', ddata[0x86:0x86 + 2])[0]  # total positions
+
+        self.car_id = struct.unpack('i', ddata[0x124:0x124 + 4])[0]  # car id
+
+        self.throttle = struct.unpack('B', ddata[0x91:0x91 + 1])[0] / 2.55  # throttle
+        self.rpm = struct.unpack('f', ddata[0x3C:0x3C + 4])[0]  # rpm
+        self.rpm_rev_warning = struct.unpack('H', ddata[0x88:0x88 + 2])[0]  # rpm rev warning
+
+        self.brake = struct.unpack('B', ddata[0x92:0x92 + 1])[0] / 2.55  # brake
+
+        self.boost = struct.unpack('f', ddata[0x50:0x50 + 4])[0] - 1  # boost
+
+        self.rpm_rev_limiter = struct.unpack('H', ddata[0x8A:0x8A + 2])[0]  # rpm rev limiter
+
+        self.estimated_top_speed = struct.unpack('h', ddata[0x8C:0x8C + 2])[0]  # estimated top speed
+
+        self.clutch = struct.unpack('f', ddata[0xF4:0xF4 + 4])[0]  # clutch
+        self.clutch_engaged = struct.unpack('f', ddata[0xF8:0xF8 + 4])[0]  # clutch engaged
+        self.rpm_after_clutch = struct.unpack('f', ddata[0xFC:0xFC + 4])[0]  # rpm after clutch
+
+        self.oil_temp = struct.unpack('f', ddata[0x5C:0x5C + 4])[0]  # oil temp
+        self.water_temp = struct.unpack('f', ddata[0x58:0x58 + 4])[0]  # water temp
+
+        self.oil_pressure = struct.unpack('f', ddata[0x54:0x54 + 4])[0]  # oil pressure
+        self.ride_height = 1000 * struct.unpack('f', ddata[0x38:0x38 + 4])[0]  # ride height
+
+        self.tyre_temp_FL = struct.unpack('f', ddata[0x60:0x60 + 4])[0]  # tyre temp FL
+        self.tyre_temp_FR = struct.unpack('f', ddata[0x64:0x64 + 4])[0]  # tyre temp FR
+
+        self.suspension_fl = struct.unpack('f', ddata[0xC4:0xC4 + 4])[0]  # suspension FL
+        self.suspension_fr = struct.unpack('f', ddata[0xC8:0xC8 + 4])[0]  # suspension FR
+
+        self.tyre_temp_rl = struct.unpack('f', ddata[0x68:0x68 + 4])[0]  # tyre temp RL
+        self.tyre_temp_rr = struct.unpack('f', ddata[0x6C:0x6C + 4])[0]  # tyre temp RR
+
+        self.suspension_rl = struct.unpack('f', ddata[0xCC:0xCC + 4])[0]  # suspension RL
+        self.suspension_rr = struct.unpack('f', ddata[0xD0:0xD0 + 4])[0]  # suspension RR
+
+        self.gear_1 = struct.unpack('f', ddata[0x104:0x104 + 4])[0]  # 1st gear
+        self.gear_2 = struct.unpack('f', ddata[0x108:0x108 + 4])[0]  # 2nd gear
+        self.gear_3 = struct.unpack('f', ddata[0x10C:0x10C + 4])[0]  # 3rd gear
+        self.gear_4 = struct.unpack('f', ddata[0x110:0x110 + 4])[0]  # 4th gear
+        self.gear_5 = struct.unpack('f', ddata[0x114:0x114 + 4])[0]  # 5th gear
+        self.gear_6 = struct.unpack('f', ddata[0x118:0x118 + 4])[0]  # 6th gear
+        self.gear_7 = struct.unpack('f', ddata[0x11C:0x11C + 4])[0]  # 7th gear
+        self.gear_8 = struct.unpack('f', ddata[0x120:0x120 + 4])[0]  # 8th gear
+
+        self.position_x = struct.unpack('f', ddata[0x04:0x04 + 4])[0]  # pos X
+        self.position_y = struct.unpack('f', ddata[0x08:0x08 + 4])[0]  # pos Y
+        self.position_z = struct.unpack('f', ddata[0x0C:0x0C + 4])[0]  # pos Z
+
+        self.velocity_x = struct.unpack('f', ddata[0x10:0x10 + 4])[0]  # velocity X
+        self.velocity_y = struct.unpack('f', ddata[0x14:0x14 + 4])[0]  # velocity Y
+        self.velocity_z = struct.unpack('f', ddata[0x18:0x18 + 4])[0]  # velocity Z
+
+        self.rotation_pitch = struct.unpack('f', ddata[0x1C:0x1C + 4])[0]  # rot Pitch
+        self.rotation_yaw = struct.unpack('f', ddata[0x20:0x20 + 4])[0]  # rot Yaw
+        self.rotation_roll = struct.unpack('f', ddata[0x24:0x24 + 4])[0]  # rot Roll
+
+        self.angular_velocity_x = struct.unpack('f', ddata[0x2C:0x2C + 4])[0]  # angular velocity X
+        self.angular_velocity_y = struct.unpack('f', ddata[0x30:0x30 + 4])[0]  # angular velocity Y
+        self.angular_velocity_z = struct.unpack('f', ddata[0x34:0x34 + 4])[0]  # angular velocity Z
+
+        self.is_paused = bin(struct.unpack('B', ddata[0x8E:0x8E + 1])[0])[-2] == '1'
+        self.in_race = bin(struct.unpack('B', ddata[0x8E:0x8E + 1])[0])[-1] == '1'
+
+
+def display_throttle_bar(throttle):
+    # Define the maximum length of the throttle bar
+    max_length = 50  # For example, a bar of 50 characters
+    # Calculate the number of characters to display
+    filled_length = int(round(throttle / 100 * max_length))
+
+    # Create the bar string
+    bar = '#' * filled_length + '-' * (max_length - filled_length)
+    
+    # Clear the line and write the output with the throttle value and bar
+    sys.stdout.write(f"\r[{bar}]")
+    time.sleep(0.1)
+    sys.stdout.flush() 
+    
+
+class Laps():
+    def __init__(self):
+        self.laps = []
+        self.best_time = self.laps[0] 
+        
+    def add_lap(self, lap):
+        self.laps.append(lap)
+        
+        if lap <= self.best_time:
+            self.best_time = lap 
+        
+
+class Lap():
+    def __init__(self):
+        self.time = 0 
+        self.x_positions = []
+        self.y_positions = []
+        self.z_positions = [] 
+        
+    def set_time(self, time):
+        self.time = time 
+        
+    def add_x(self, x):
+        self.x_positions.append(x) 
+        
+    def add_y(self, y):
+        self.y_positions.append(y)
+        
+    def add_z(self, z):
+        self.z_positions.append(z) 
+
+        
+
+#TODO: implement threading here, to prevent timeout. 
+class GT7Comms(Thread):
+    """ Sets up the communication between the Playstation and the Computer """
+    def __init__(self):
+        Thread.__init__(self) 
+        self.send_port = 33739
+        self.receive_port = 33740 
+        self.playstation_ip = "255.255.255.255" 
+        self.daemon = True  
+        self._shall_run = True 
+        self.package_number = 0 
+        self.currlap = 0 
+            
+    def run(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        s.bind(('0.0.0.0', self.receive_port))
+        self._send_hb(s)
+
+        laps = []  # Initialize outside the loop if laps should persist across packets
+        while self._shall_run:
+            data, address = s.recvfrom(4096)
+            self.package_number += 1
+
+            if self.package_number > 100:
+                self._send_hb(s)
+                self.package_number = 0
+
+            ddata = salsa20_dec(data)
+            if len(ddata) > 0 and struct.unpack('i', ddata[0x70:0x70 + 4])[0] > 0:
+                current_data = Data(ddata)
+                
+                sys.stdout.write(f"\rThrottle : {current_data.throttle}")
+                sys.stdout.flush()
+                
+                    
+                """
+                if current_data.current_lap > self.currlap:
+                    current_lap = Lap()  # Only instantiate a new Lap if it's a new lap
+                    laps.append(current_lap)
+                    self.currlap += 1
+                    
+                    current_lap.add_x(current_data.position_x)
+                    current_lap.add_y(current_data.position_y)
+                    current_lap.add_z(current_data.position_z)
+                    
+                    # Update current_data here (not shown: ensure new data is processed) 
+                """  
+                
+    def stop(self):
+        self._shall_run = False 
+    
+    #send a heartbeat to the PlayStation so it doesn't think we are inactive. 
+    def _send_hb(self, s):
+        send_data = 'A'
+        s.sendto(send_data.encode('utf-8'), (self.playstation_ip, self.send_port)) 
+            
+
+# data stream decoding
+def salsa20_dec(dat):
+    try:
+        key = b'Simulator Interface Packet GT7 ver 0.0'
+        # Seed IV is always located here
+        oiv = dat[0x40:0x44]
+        iv1 = int.from_bytes(oiv, byteorder='little')
+        # Notice DEADBEAF, not DEADBEEF
+        iv2 = iv1 ^ 0xDEADBEAF
+        iv = bytearray()
+        iv.extend(iv2.to_bytes(4, 'little'))
+        iv.extend(iv1.to_bytes(4, 'little'))
+        ddata = Salsa20_xor(dat, bytes(iv), key[0:32])
+        magic = int.from_bytes(ddata[0:4], byteorder='little')
+        if magic != 0x47375330:
+            return bytearray(b'')
+        return ddata
+    except Exception as e:
+        print("ERROR")
+
+if __name__ == "__main__":
+    gt7comms = GT7Comms() 
+    gt7comms.start() 
+    gt7comms.join() 
+    
+    
